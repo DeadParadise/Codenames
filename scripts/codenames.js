de749bace8473697b971e134fed42d60fb3f7c75
mgomez <manoloelgomo@gmail.com>
2020-05-06 13:51:29 -0500

    Added CSS card colors and JS for swtiching between them

---
+++ scripts/codenames.js
@@ -0,0 +1,549 @@
+
+var button = document.getElementById("card-1");
+
+
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-1").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-1").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-1").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-1").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-1").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-1").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-2");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-2").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-2").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-2").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-2").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-2").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-2").style.backgroundColor = "firebrick";
+    }
+});
+var button = document.getElementById("card-3");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-3").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-3").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-3").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-3").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-3").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-3").style.backgroundColor = "firebrick";
+    }
+});
+var button = document.getElementById("card-4");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-4").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-4").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-4").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-4").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-4").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-4").style.backgroundColor = "firebrick";
+    }
+});
+var button = document.getElementById("card-5");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-5").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-5").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-5").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-5").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-5").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-5").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-6");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-6").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-6").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-6").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-6").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-6").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-6").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-7");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-7").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-7").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-7").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-7").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-7").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-7").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-8");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-8").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-8").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-8").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-8").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-8").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-8").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-9");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-9").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-9").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-9").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-9").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-9").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-9").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-10");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-10").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-10").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-10").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-10").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-10").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-10").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-11");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-11").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-11").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-11").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-11").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-11").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-11").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-12");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-12").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-12").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-12").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-12").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-12").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-12").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-13");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-13").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-13").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-13").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-13").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-13").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-13").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-14");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-14").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-14").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-14").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-14").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-14").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-14").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-15");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-15").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-15").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-15").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-15").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-15").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-15").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-16");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-16").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-16").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-16").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-16").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-16").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-16").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-17");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-17").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-17").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-17").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-17").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-17").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-17").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-18");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-18").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-18").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-18").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-18").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-18").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-18").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-19");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-19").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-19").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-19").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-19").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-19").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-19").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-20");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-20").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-20").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-20").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-20").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-20").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-20").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-21");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-21").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-21").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-21").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-21").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-21").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-21").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-22");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-22").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-22").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-22").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-22").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-22").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-22").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-23");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-23").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-23").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-23").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-23").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-23").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-23").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-24");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-24").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-24").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-24").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-24").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-24").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-24").style.backgroundColor = "firebrick";
+    }
+});
+
+var button = document.getElementById("card-25");
+
+button.addEventListener("click", function() {
+    const curColour = document.getElementById("card-25").style.backgroundColor;
+
+    if (curColour === 'firebrick') {
+        document.getElementById("card-25").style.backgroundColor = "teal";
+    }
+      else if (curColour === 'teal') {
+        document.getElementById("card-25").style.backgroundColor = "gray";
+    }
+      else if (curColour === 'gray') {
+        document.getElementById("card-25").style.backgroundColor = "black";
+    }
+      else if (curColour === 'black') {
+        document.getElementById("card-25").style.backgroundColor = "#C78E00";
+    }
+    else {
+        document.getElementById("card-25").style.backgroundColor = "firebrick";
+    }
+});
